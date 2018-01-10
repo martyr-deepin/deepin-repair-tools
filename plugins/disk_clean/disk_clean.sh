37516209af577d0b126de3837868e6cda5a7b84b
石博文 <sbw@sbw.so>
2018-01-10 14:05:15 +0800

    fix clean size calculate error
    Change-Id: Ie21d9fc2f0e58471aec728ecb25a2ca5fe6791ac

--- plugins/disk_clean/disk_clean.sh
+++ plugins/disk_clean/disk_clean.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+if [ ! -f /var/cache/apt/archives/*.deb ]; then
+    exit 0
+fi
 
 # size calculate
-printf "DiskClean: %s\n" "`du -sh /var/cache/apt/archives/*.deb`"
+printf "DiskClean: %s\n" "`du -sh /var/cache/apt/archives`"
 
 apt clean
