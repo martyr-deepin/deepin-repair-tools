edbc32bf9759e703acd6b2c96ddb4f179b4a96ed
leaeasy <leaeasy@gmail.com>
2018-03-02 09:39:22 +0800

    fix disk_clean plugin
    Change-Id: I8fe50491cb326b06c1c87d3cf6197f213479ccc7

--- plugins/disk_clean/disk_clean.sh
+++ plugins/disk_clean/disk_clean.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-if [ ! -f /var/cache/apt/archives/*.deb ]; then
-    exit 0
-fi
+# /var/cache/apt/archives
+# /var/lib/apt/lists ~ 181M
+# /var/cache/apt/pkgcache.bin /var/cache/apt/srcpkgcache.bin ~ 181M
+# /var/lib/lastore/safecache ~ 200M
 
 # size calculate
-printf "DiskClean: %s\n" "`du -sh /var/cache/apt/archives`"
+TOTAL_SIZE=$(LC_ALL=C find /var/lib/apt/lists /var/cache/apt /var/lib/lastore/safecache  -type f -not -name lock -exec du -ch {} + | grep total)
+if [ -z ${TOTAL_SIZE} ]; then
+	printf "Exit: Already DiskClean.\n" ; exit 0
+fi
 
-apt clean
+printf "DiskClean: %s\n" ${TOTAL_SIZE}
+LC_ALL=C find /var/lib/apt/lists /var/cache/apt /var/lib/lastore/safecache -type f -not -name lock -exec rm -vf {} +
